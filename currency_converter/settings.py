6c8b24769e35382138f9881cc726199acd47f799
Yuri Issamoto <yuh.tomodaty@hotmail.com>
2024-03-02 23:57:29 -0300

    FirstCommit

--- currency_converter/settings.py
+++ currency_converter/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-yjxb#9$9u$1^+p*e4bxtxc-(091dcyek+uri$*njm6j(3$j)(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
